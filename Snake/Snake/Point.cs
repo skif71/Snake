fbbbc20d3879e2713ccbac6b704048439f344d03
skif71 <48547695+skif71@users.noreply.github.com>
2020-02-17 22:01:50 +0200

    Class Point Added

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        public void Draw()
+        {
+
+        }
+    }
+}
