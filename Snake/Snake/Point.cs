ca7b66deda11ea4a3d030785e2c8eb7a47090070
skif71 <48547695+skif71@users.noreply.github.com>
2020-02-19 20:55:06 +0200

    Constructor

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -9,9 +9,18 @@ namespace Snake
         public int x;
         public int y;
         public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
-
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
